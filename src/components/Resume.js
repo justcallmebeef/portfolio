97fd3e93d6a62fdfe6096e399e34b02e59515921
justcallmebeef <bethanymitch10@gmail.com>
2018-12-04 10:39:19 -0700

    added over states over my links on projects and resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -20,7 +20,9 @@ const Resume = (props) => {
             </nav>
             <div className="resumeDiv">
             <a href="https://docs.google.com/document/d/16n3BzpKDFfn1SKDxJNaQt1lctZnLB_1tllDK-_vL89c/export?format=pdf">
-                    <div className="resumeButton">Download Bethany's Resume</div>
+                    <div className="resumeButton">Download Bethany's Resume
+                        <i id="resumeIcon" class="fa fa-download"></i>
+                    </div>
                 </a>
                 <img className="resume" src={resume} alt="resume" />
             </div>
