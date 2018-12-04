21bb9d58b65e0d6f93e6f8f2b538ff603d49d2d2
justcallmebeef <bethanymitch10@gmail.com>
2018-12-04 11:47:52 -0700

    added hover color to links on contact page, deleted home page component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,10 +18,10 @@ const Contact = (props) => {
             </ul>
             </nav>
             <div className="contactDiv">
-            <i id="bigIcon" className="fa fa-envelope"></i><p>bethanymitch10@gmail.com</p>
+            <a href= "mailto:your_email" rel="noopener noreferrer"><i id="bigIcon" className="fa fa-envelope"></i></a><a href= "mailto:your_email" rel="noopener noreferrer"><p>bethanymitch10@gmail.com</p></a>
             <a href="https://www.linkedin.com/in/bethany-mitch/" rel="noopener noreferrer" target="_blank"><i id="bigIcon" className="fa fa-linkedin-square"></i></a><a href="https://www.linkedin.com/in/bethany-mitch/" rel="noopener noreferrer" target="_blank"><p>/in/bethany-mitch/</p></a>
             <a href="https://github.com/justcallmebeef" rel="noopener noreferrer" target="_blank"><i id="bigIcon" className="fa fa-github"></i></a><a href="https://github.com/justcallmebeef" rel="noopener noreferrer" target="_blank"><p>/justcallmebeef</p></a>
-            <i id="bigIcon" className="fa fa-mobile"></i><p>(715) 688-9727</p>
+            <i id="bigIconPhone" className="fa fa-mobile"></i><p>(715) 688-9727</p>
             </div>
         </Fragment>
     )
