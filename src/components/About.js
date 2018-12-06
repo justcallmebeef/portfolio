771768fc74bab63d0c05b3791cf238b35ce53491
justcallmebeef <bethanymitch10@gmail.com>
2018-12-05 20:05:53 -0700

    added image to about section and parallax effect along with div w/ border containing quick facts about myself

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,8 @@ import React, { Fragment } from 'react'
 import { Link } from 'react-router-dom'
 
 
+// photo link: https://images.unsplash.com/photo-1469122312224-c5846569feb1?ixlib=rb-1.2.1&auto=format&fit=crop&w=1152&q=80
+// computer: https://images.unsplash.com/photo-1527234639945-70d78416bd7d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1019&q=80
 const About = (props) => {
     return (
         <Fragment>
@@ -17,13 +19,30 @@ const About = (props) => {
                 <li><Link to='/contact' style={{ textDecoration: 'none', color: '#008080' }}>Contact</Link></li>
             </ul>
             </nav>
+            <div className="parallax">
+            </div>
             <div className="aboutDiv">
-            <p className="projectP">
-                Hello again! Sorry to report this page is still a work in progress.<br></br>
-                Please come back later or visit the Contact page and reach out.<br></br>
-                <br></br>
-                Have a great day!
-            </p>
+                <section className="section">
+                <p>Quick Fun Facts: 
+                <ul>
+                    <li>My nickname is Beef</li>
+                    <li>Born and raised in Wisconsin - a true cheesehead at heart</li>
+                    <li>BA in Mass Communication with a minor in Computer Science</li>
+                    <li>Studied abroad in Scotland</li>
+                    <li>Lived in Minneapolis, MN while working in Advertising</li>
+                    <li>Moved to Colorado to work for Avid 4 Adventure</li>
+                    <li>After hours:
+                        <ul>
+                            <li>Rock Climbing</li>
+                            <li>Ultimate Frisbee</li>
+                            <li>Volleyball</li>
+                            <li>Camping</li>
+                            <li>Reading</li>
+                        </ul>
+                    </li>
+                </ul>
+                </p>
+                </section>
             </div>
         </Fragment>
     )
