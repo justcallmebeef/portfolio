ff07e25f33c0d22d986c8314417c9651107e3b82
justcallmebeef <bethanymitch10@gmail.com>
2018-12-06 09:01:05 -0700

    lowered zoom down to 115% and udpated email contact button to my actual email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -27,7 +27,7 @@ const Contact = (props) => {
             </ul>
             </nav>
             <div className="contactDiv">
-            <a href= "mailto:your_email" rel="noopener noreferrer"><i id="bigIcon" className="fa fa-envelope"></i></a><a href= "mailto:your_email" rel="noopener noreferrer"><p>bethanymitch10@gmail.com</p></a>
+            <a href= "mailto:your_email" rel="noopener noreferrer"><i id="bigIcon" className="fa fa-envelope"></i></a><a href= "mailto:bethanymitch10@gmail.com" rel="noopener noreferrer"><p>bethanymitch10@gmail.com</p></a>
             <a href="https://www.linkedin.com/in/bethany-mitch/" rel="noopener noreferrer" target="_blank"><i id="bigIcon" className="fa fa-linkedin-square"></i></a><a href="https://www.linkedin.com/in/bethany-mitch/" rel="noopener noreferrer" target="_blank"><p>/in/bethany-mitch/</p></a>
             <a href="https://github.com/justcallmebeef" rel="noopener noreferrer" target="_blank"><i id="bigIcon" className="fa fa-github"></i></a><a href="https://github.com/justcallmebeef" rel="noopener noreferrer" target="_blank"><p>/justcallmebeef</p></a>
             <i id="bigIconPhone" className="fa fa-mobile"></i><p>(715) 688-9727</p>
