4d0b5f172024fd28f693ea64db9cdc745422c18d
justcallmebeef <bethanymitch10@gmail.com>
2018-12-07 18:12:34 -0700

    added full name to logo with an opacity change on hover

--- src/components/NavDesktop.js
+++ src/components/NavDesktop.js
@@ -5,15 +5,20 @@ import { Link, NavLink } from 'react-router-dom'
 const NavDesktop = () => {
     return (
         <nav id="bigScreen" className="navBar">
-            <p id="logo">
-                <Link to='/' style={{ textDecoration: 'none', color: '#008080' }}>bpm</Link>
-            </p>
+            <div className="logoContainer">
+                <p id="logo">
+                    <Link to='/' style={{ textDecoration: 'none', color: '#008080' }}>bpm</Link>
+                </p>
+                <p className="logoName">bethany p. mitch</p>
+            </div>
+            <div>
             <ul className="mainNav">
                 <li><NavLink exact={true} activeClassName="selected" to='/' style={{ textDecoration: 'none', color: '#008080' }}>Projects</NavLink></li>
                 <li><NavLink activeClassName="selected" to='/resume' style={{ textDecoration: 'none', color: '#008080' }}>Resume</NavLink></li>
                 <li><NavLink activeClassName="selected" to='/about' style={{ textDecoration: 'none', color: '#008080' }}>About</NavLink></li>
                 <li><NavLink activeClassName="selected" to='/contact' style={{ textDecoration: 'none', color: '#008080' }}>Contact</NavLink></li>
             </ul>
+            </div>
         </nav>
     )
 }
--- src/components/NavMobile.js
+++ src/components/NavMobile.js
@@ -8,8 +8,12 @@ const NavMobile = (props) => {
             <span className="navBar-toggle">
                 <i onClick={props.navBarResponsive} id="icon" className="fa fa-bars"></i>
             </span>
-            <p id="logo"><Link to='/' style={{ textDecoration: 'none', color: '#008080' }}>bpm</Link></p>
-            <ul className={`${props.toggleMenu ? 'mainNav-none' : 'mainNav' }`}>
+            <div className="logoContainer">
+                <p id="logo">
+                    <Link to='/' style={{ textDecoration: 'none', color: '#008080' }}>bpm</Link>
+                </p>
+                <p className="logoName">bethany p. mitch</p>
+            </div>            <ul className={`${props.toggleMenu ? 'mainNav-none' : 'mainNav' }`}>
                 <li><NavLink exact={true} activeClassName="selected" to='/' style={{ textDecoration: 'none', color: '#008080' }}>Projects</NavLink></li>
                 <li><NavLink activeClassName="selected" to='/resume' style={{ textDecoration: 'none', color: '#008080' }}>Resume</NavLink></li>
                 <li><NavLink activeClassName="selected" to='/about' style={{ textDecoration: 'none', color: '#008080' }}>About</NavLink></li>
