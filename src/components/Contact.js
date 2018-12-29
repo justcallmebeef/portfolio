d59af708ed8ab9d9830a819a30475959ce696d2b
justcallmebeef <bethanymitch10@gmail.com>
2018-12-29 15:22:34 -0700

    removed phone number from contact page and removed commented code

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,34 +5,12 @@ import NavMobile from './NavMobile'
 const Contact = (props) => {
     return (
         <Fragment>
-            {/* <nav id="littleScreen" className="navBar">
-                <span className="navBar-toggle">
-                    <i onClick={props.navBarResponsive} id="icon" className="fa fa-bars"></i>
-                </span>
-            <p id="logo"><Link to='/' style={{ textDecoration: 'none', color: '#008080' }}>bpm</Link></p>
-            <ul className={`${props.toggleMenu ? 'mainNav' : 'mainNav-none' }`}>
-                <li><Link to='/' style={{ textDecoration: 'none', color: '#008080' }}>Projects</Link></li>
-                <li><Link to='/resume' style={{ textDecoration: 'none', color: '#008080' }}>Resume</Link></li>
-                <li><Link to='/about' style={{ textDecoration: 'none', color: '#008080' }}>About</Link></li>
-                <li className="active"><Link to='/contact' style={{ textDecoration: 'none', color: '#008080' }}>Contact</Link></li>
-            </ul>
-            </nav>
-            <nav id="bigScreen" className="navBar">
-            <p id="logo"><Link to='/' style={{ textDecoration: 'none', color: '#008080' }}>bpm</Link></p>
-            <ul className={`${props.toggleMenu ? 'mainNav' : 'mainNav-none' }`}>
-                <li><Link to='/' style={{ textDecoration: 'none', color: '#008080' }}>Projects</Link></li>
-                <li><Link to='/resume' style={{ textDecoration: 'none', color: '#008080' }}>Resume</Link></li>
-                <li><Link to='/about' style={{ textDecoration: 'none', color: '#008080' }}>About</Link></li>
-                <li className="active"><Link to='/contact' style={{ textDecoration: 'none', color: '#008080' }}>Contact</Link></li>
-            </ul>
-            </nav> */}
             <NavDesktop />
             <NavMobile toggleMenu={props.toggleMenu} navBarResponsive={props.navBarResponsive} />
             <div className="contactDiv">
             <a href= "mailto:bethanymitch10@gmail.com" rel="noopener noreferrer"><i id="bigIcon" className="fa fa-envelope"></i></a><a href= "mailto:bethanymitch10@gmail.com" rel="noopener noreferrer"><p>bethanymitch10@gmail.com</p></a>
             <a href="https://www.linkedin.com/in/bethany-mitch/" rel="noopener noreferrer" target="_blank"><i id="bigIcon" className="fa fa-linkedin-square"></i></a><a href="https://www.linkedin.com/in/bethany-mitch/" rel="noopener noreferrer" target="_blank"><p>/in/bethany-mitch/</p></a>
             <a href="https://github.com/justcallmebeef" rel="noopener noreferrer" target="_blank"><i id="bigIcon" className="fa fa-github"></i></a><a href="https://github.com/justcallmebeef" rel="noopener noreferrer" target="_blank"><p>/justcallmebeef</p></a>
-            <i id="bigIconPhone" className="fa fa-mobile"></i><p>(715) 688-9727</p>
             </div>
         </Fragment>
     )
